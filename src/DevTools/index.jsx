24bff5f2a4028076e1b621bb3ac69db5d9323a03
fuhao <465476662@qq.com>
2016-07-25 17:41:16 +0800

    增加不同的DevTools

--- src/DevTools/index.jsx
+++ src/DevTools/index.jsx
@@ -6,13 +6,18 @@ import DockMonitor from 'redux-devtools-dock-monitor';
 import LogMonitor from 'redux-devtools-log-monitor';
 //另一种Log
 import FilterableLogMonitor from 'redux-devtools-filterable-log-monitor';
+import ChartMonitor from 'redux-devtools-chart-monitor';
+import SliderMonitor from 'redux-slider-monitor';
+import DiffMonitor from 'redux-devtools-diff-monitor';
 
 const DevTools = createDevTools(
+
     <DockMonitor toggleVisibilityKey='ctrl-h'
                  changePositionKey='ctrl-q'
                  defaultIsVisible={true}>
         <LogMonitor theme='tomorrow'/>
     </DockMonitor>
+    
 );
 
 export default DevTools;
