f1cc64553c25dea3f829f85730cfd6aae5b071de
fuhao <465476662@qq.com>
2016-07-29 14:44:58 +0800

    整理项目目录

--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import * as goods from 'src/Shop/goods';
-import * as cart from 'src/Shop/cart';
+import * as Good from 'src/Good';
+import * as Cart from 'src/Cart';
 
 export default combineReducers({
-    [goods.constants.NAME]: goods.reducer,
-    [cart.constants.NAME]: cart.reducer,
+    [Good.constants.NAME]: Good.reducer,
+    [Cart.constants.NAME]: Cart.reducer,
     routing: routerReducer
 });
