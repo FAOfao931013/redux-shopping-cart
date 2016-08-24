83a1cddf18a78e8d3d432a34d7df9405c068ce06
fuhao <465476662@qq.com>
2016-08-24 12:09:25 +0800

    增加Header点击样式

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { push, go } from 'react-router-redux';
+import './style.less'
 
 class Header extends React.Component {
     constructor(props) {
@@ -36,11 +37,12 @@ class Header extends React.Component {
                 <div className="navbar">
                     <div className="navbar-inner">
                         <div
-                            className="left"
-                            onClick={() => this.go(left.event)}>{left.name}</div>
+                            className="left link"
+                            onClick={() => this.go(left.event)}>{left.name}
+                        </div>
                         <div className="center">{center}</div>
                         <div
-                            className="right"
+                            className="right link"
                             onClick={() => this.goto(right.url)}>{right.name}</div>
                     </div>
                 </div>
